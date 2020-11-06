81079e3ff408ebb2a9a1d7a2f9f7fe2415c9faad
sergeyhambardzumyan <hserog@yahoo.com>
2020-11-06 03:09:01 -0800

    Homework 1.1

---
+++ src/com/company/Homework1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int num1 = 74;
+        int num2 = 36;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
